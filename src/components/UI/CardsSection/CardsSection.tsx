f49f9f6ca29ce8997c24d664ca194c96fee7a9c7
Jorge Garcia Ojeda <jor4300@gmail.com>
2025-11-21 13:27:02 -0300

    cambio de las imagenes de la landing

--- src/components/UI/CardsSection/CardsSection.tsx
+++ src/components/UI/CardsSection/CardsSection.tsx
@@ -1,11 +1,19 @@
 import styles from "./CardsSection.module.css";
-import casaCard from "../../../assets/img_card1.jpg";
-import contact from "../../../assets/Casa Garitaonandia-33.png";
+import casaCard from "../../../assets/imgCards.jpeg";
+
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export const CardsSection = () => {
   const phone = import.meta.env.VITE_CONTACT_PHONE;
   const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 480);
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 480);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleGoProperties = () => {
     navigate("/propiedades");
@@ -41,26 +49,19 @@ export const CardsSection = () => {
           </Button>
         </div>
       </div>
-
-      <div className={styles.card}>
-        <div className={styles.cardInfoContainer2}>
-          <h5 className={styles.cardTitle}>Quienes somos!</h5>
-          <p className={styles.cardText}>
-            En casa Garitaonandia estamos comprometidos con brindar un servicio
-            cercano, transparente y profesional. Nuestro objetivo es ayudarte a
-            encontrar la propiedad ideal para vos y tu familia, ofreciendo
-            acompañamiento en cada paso del proceso de compra, venta o alquiler.
+      <div className={styles.cardAlt}>
+        <div className={styles.cardAltContent}>
+          <h5 className={styles.cardTitle}>
+            {isMobile ? "Conecta con nosotros!" : "Quienes somos!"}
+          </h5>
+          <p className={styles.cardTextAlt}>
+            {isMobile
+              ? "Contactanos para recibir aseoramiento personalizado en tu búsqueda de propiedades."
+              : "En casa Garitaonandia estamos comprometidos con brindar un servicio cercano, transparente y profesional. Nuestro objetivo es ayudarte a encontrar la propiedad ideal para vos y tu familia, ofreciendo acompañamiento en cada paso del proceso de compra, venta o alquiler."}
           </p>
           <Button className={styles.buttonPropiedades} onClick={handleContacto}>
             Contactanos!
           </Button>
-        </div>
-        <div className={styles.cardImgContainer}>
-          <img
-            src={contact}
-            alt="Properties Image"
-            className={styles.cardImg}
-          />
         </div>
       </div>
     </div>
